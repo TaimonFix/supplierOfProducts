90e55f77f98a937060a00ddf5ff82c423085795a
TaimonFix <bratyshev2013@mail.ru>
2024-05-23 09:49:22 +0300

    Добавлен Spring Boot

---
+++ src/main/java/com/bratyshevTD/supplierOfProducts/SupplierOfProductsApplication.java
@@ -0,0 +1,13 @@
+package com.bratyshevTD.supplierOfProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierOfProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierOfProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bratyshevTD/supplierOfProducts/SupplierOfProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bratyshevTD.supplierOfProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierOfProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
